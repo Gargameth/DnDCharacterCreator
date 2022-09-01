0f3770fdfe73315f03cd2c6580f1e75b7417beed
Gargameth <stragorhelm@gmail.com>
2022-09-01 15:41:35 +0200

    Add choosing of initial skills after class selection.

--- DnDCharacterGenerator/CharacterClass.cs
+++ DnDCharacterGenerator/CharacterClass.cs
@@ -9,6 +9,7 @@ namespace DnDCharacterGenerator
     internal class CharacterClass
     {
         private string characterClass;
+        private List<string> classSkills = new List<string>();
 
         public CharacterClass(int charactersClass)
         {
@@ -62,11 +63,122 @@ namespace DnDCharacterGenerator
                     characterClass = "Wizard";
                     break;
             }
+            
+            SetClassSkills();
+        }
+
+        private void SetClassSkills()
+        {
+            Console.WriteLine("Set your first class skill (try not make typos; letters are case sensitive): \n");
+            List<string> dummyClassSkills = new List<string>();
+            switch (characterClass)
+            {
+                case "Barbarian":
+                    classSkills.Add("Animal Handling");
+                    classSkills.Add("Athletics");
+                    classSkills.Add("Intimidation");
+                    classSkills.Add("Nature");
+                    classSkills.Add("Perception");
+                    
+                    foreach(string skill in classSkills)
+                    {
+                        Console.WriteLine(skill);
+                    }
+
+                    var firstInput = Console.ReadLine();
+                    while (dummyClassSkills.Count == 0)
+                    {
+                        if (classSkills.Contains(firstInput))
+                        {
+                            dummyClassSkills.Add(firstInput);
+                            classSkills.Remove(firstInput);
+                        }
+                        else
+                        {
+                            Console.WriteLine("I told you to watch them typos... ya donkey! Try again.");
+                            firstInput = Console.ReadLine();
+                        }
+                    }
+                    
+                    Console.Clear();
+                    Console.WriteLine("Now choose your second class skill (with the above mentiones typing rules): \n");
+                    foreach (string skill in classSkills)
+                    {
+                        Console.WriteLine(skill);
+                    }
+                    var secondInput = Console.ReadLine();
+                    while (dummyClassSkills.Count == 1)
+                    {
+                        if (classSkills.Contains(secondInput))
+                        {
+                            dummyClassSkills.Add(secondInput);
+                        }
+                        else
+                        {
+                            Console.WriteLine("I told you to watch them typos... ya donkey! Try again.");
+                            secondInput = Console.ReadLine();
+                        }
+                    }
+                    foreach (string skill in classSkills)
+                    {
+                        Console.WriteLine(skill);
+                    }
+
+                    Console.Clear();
+
+                    break;
+
+                case "Bard":
+                    break;
+
+                case "Cleric":
+                    break;
+
+                case "Druid":
+                    break;
+
+                case "Fighter":
+                    break;
+
+                case "Monk":
+                    break;
+
+                case "Paladin":
+                    break;
+
+                case "Ranger":
+                    break;
+
+                case "Rogue":
+                    break;
+
+                case "Sorcerer":
+                    break;
+
+                case "Warlock":
+                    break;
+
+                case "Wizard":
+                    break;
+            }
+
+            classSkills = dummyClassSkills;
+            foreach(string skill in classSkills)
+            {
+                Console.WriteLine(skill);
+            }
+            Console.ReadLine();
+
         }
 
         public string ReturnCharacterClass()
         {
             return characterClass;
+        }
+
+        public List<string> ReturnClassSkills()
+        {
+            return classSkills;
         }
     }
 }
